ba71ced11f812181bfbefffee71d72203f40c6fc
Carolina Gonzalez <carogonzalezleal@hotmail.com>
2025-10-06 07:56:49 -0600

    Added Apple Watch Target

---
+++ BadgelyWatch Watch App/BadgelyWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadgelyWatchApp.swift
+//  BadgelyWatch Watch App
+//
+//  Created by Carolina Nicole Gonzalez Leal on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BadgelyWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
