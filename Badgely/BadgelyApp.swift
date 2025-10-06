0a8aba9819eea621687b9b150615d28ce75e9e18
Carolina Gonzalez <carogonzalezleal@hotmail.com>
2025-10-06 07:04:09 -0600

    Initial Commit

---
+++ Badgely/BadgelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadgelyApp.swift
+//  Badgely
+//
+//  Created by Carolina Nicole Gonzalez Leal on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BadgelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
