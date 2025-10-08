4e74f8bac59afbc88d0e136a0518477d86b4b6f0
Martha Mendoza <mmarthamendozaa@gmail.com>
2025-10-08 12:54:44 -0600

    Swiftdata foundations

--- Badgely/Models/User.swift
+++ Badgely/Models/User.swift
@@ -7,16 +7,40 @@
 import SwiftUI
 import SwiftData
 
+//diccionario todas las categorias,
+
 // solo para el user y sus badges
 @Model
 class User {
     var name: String
     var city: String
-    var joinDate: Date
+    var avatarName: String
+    
+    
 
-    init(name: String, city: String, joinDate: Date) {
+    init(name: String, city: String, avatarnName: String ) {
         self.name = name
         self.city = city
-        self.joinDate = joinDate
+        self.avatarName = avatarnName
     }
+    
 }
+
+class Badge {
+    var name: String
+    var city: String
+    var category: String
+    var imageName: String
+    var date: Date
+    
+
+    init(name: String, city: String, category: String, imageName: String, date: Date ) {
+        self.name = name
+        self.city = city
+        self.category = category
+        self.imageName = imageName
+        self.date = date
+    }
+    
+}
+
